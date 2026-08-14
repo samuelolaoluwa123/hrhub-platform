af7ab8d182480ba2a5c4913950f1bc59b2ff743d
samuelolaoluwa123 <nwosasamuel123@gmail.com>
2026-08-14 10:33:29 +0100

    Added unauthorized page

---
+++ app/unauthorized/page.js
@@ -0,0 +1,9 @@
+export default function UnauthorizedPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <p className="text-[var(--color-text-primary)]">
+        You don't have access to this page.
+      </p>
+    </main>
+  );
+}
